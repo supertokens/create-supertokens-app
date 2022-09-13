7bf6182148b6a8be7c46d9ce4f22bcc57b8c7963
Nemi Shah <nemishah1212@gmail.com>
2022-09-13 18:27:01 +0530

    Update Vue boilerplate

---
+++ boilerplate/frontend/vue-prebuilt/src/views/AuthView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import React from "react";
+import SuperTokensReactComponent from "../components/SuperTokens";
+import ReactDOM from "react-dom";
+
+export default {
+    mounted: () => {
+        ReactDOM.render(React.createElement(SuperTokensReactComponent), document.getElementById("authapp"))
+    },
+}
+</script>
+
+<template>
+        <div id="authapp"/>
+</template>
+    
