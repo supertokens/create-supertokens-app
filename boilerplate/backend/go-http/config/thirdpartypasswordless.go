dd36fa6bbca803c888a508e070ee6375d2f0e5d8
Nemi Shah <nemishah1212@gmail.com>
2022-09-14 11:28:24 +0530

    Modify config for go tpp

--- boilerplate/backend/go-http/config/thirdpartypasswordless.go
+++ boilerplate/backend/go-http/config/thirdpartypasswordless.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/supertokens/supertokens-golang/recipe/passwordless/plessmodels"
 	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/recipe/thirdparty"
+	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
 	"github.com/supertokens/supertokens-golang/recipe/thirdpartypasswordless"
 	"github.com/supertokens/supertokens-golang/recipe/thirdpartypasswordless/tplmodels"
 	"github.com/supertokens/supertokens-golang/supertokens"
@@ -23,7 +25,24 @@ var SuperTokensConfig = supertokens.TypeInput{
 			ContactMethodEmailOrPhone: plessmodels.ContactMethodEmailOrPhoneConfig{
 				Enabled: true,
 			},
-			/*TODO: See next steps for third party provider setup */
+			Providers: [] tpmodels.TypeProvider{
+				thirdparty.Google(tpmodels.GoogleConfig{
+					ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+					ClientSecret: "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",
+				}),
+				thirdparty.Github(tpmodels.GithubConfig{
+					ClientID:     "467101b197249757c71f",
+					ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+				}),
+				thirdparty.Apple(tpmodels.AppleConfig{
+					ClientID: "4398792-io.supertokens.example.service",
+					ClientSecret: tpmodels.AppleClientSecret{
+						KeyId:      "7M48Y4RYDL",
+						PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+						TeamId:     "YWQCXGJRJL",
+					},
+				}),
+			},
 		}),
 		session.Init(nil), // initializes session features
 	},
