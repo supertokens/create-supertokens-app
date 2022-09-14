c770bd7dd62c0962b0d14ec9dd5bce43f6e6debf
Nemi Shah <nemishah1212@gmail.com>
2022-09-14 12:53:18 +0530

    Change port of nest boilerplate

--- boilerplate/backend/nest/src/main.ts
+++ boilerplate/backend/nest/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalFilters(new SupertokensExceptionFilter());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
