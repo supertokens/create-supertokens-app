c77b42959da7fe886faf00cf8ca303b1fbcd5781
Nemi Shah <nemishah1212@gmail.com>
2022-09-16 15:52:35 +0530

    Add thirdpartyconfig to all backend

---
+++ boilerplate/backend/go-http/config/thirdparty.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/recipe/thirdparty"
+	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
+var SuperTokensConfig = supertokens.TypeInput{
+	Supertokens: &supertokens.ConnectionInfo{
+		ConnectionURI: "https://try.supertokens.io",
+	},
+	AppInfo: supertokens.AppInfo{
+		AppName:       "SuperTokens Demo App",
+		APIDomain:     "http://localhost:3001",
+		WebsiteDomain: "http://localhost:3000",
+	},
+	RecipeList: []supertokens.Recipe{
+		thirdparty.Init(&tpmodels.TypeInput{
+			SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
+				Providers: []tpmodels.TypeProvider{
+					// We have provided you with development keys which you can use for testing.
+					// IMPORTANT: Please replace them with your own OAuth keys for production use.
+					thirdparty.Google(tpmodels.GoogleConfig{
+						ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+						ClientSecret: "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",
+					}),
+					thirdparty.Github(tpmodels.GithubConfig{
+						ClientID:     "467101b197249757c71f",
+						ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+					}),
+					thirdparty.Apple(tpmodels.AppleConfig{
+						ClientID: "4398792-io.supertokens.example.service",
+						ClientSecret: tpmodels.AppleClientSecret{
+							KeyId:      "7M48Y4RYDL",
+							PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+							TeamId:     "YWQCXGJRJL",
+						},
+					}),
+				},
+			},
+		}),
+		session.Init(nil), // initializes session features
+	},
+}
