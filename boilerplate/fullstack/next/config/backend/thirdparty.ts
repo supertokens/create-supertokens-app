f9499b7f04c3f78b55ee5b43a95625991c656bc9
Nemi Shah <nemishah1212@gmail.com>
2022-09-16 16:03:36 +0530

    Add thirdpartyconfig to next fullstacj

---
+++ boilerplate/fullstack/next/config/backend/thirdparty.ts
@@ -0,0 +1,45 @@
+import ThirdPartyNode from 'supertokens-node/recipe/thirdparty'
+import SessionNode from 'supertokens-node/recipe/session'
+import { appInfo } from './appInfo'
+import { AuthConfig } from '../interfaces'
+
+export let backendConfig = (): AuthConfig => {
+  return {
+    framework: 'express',
+    supertokens: {
+      // this is the location of the SuperTokens core.
+      connectionURI: 'https://try.supertokens.io',
+    },
+    appInfo,
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [
+        ThirdPartyNode.init({
+            signInAndUpFeature: {
+                providers: [
+                    // We have provided you with development keys which you can use for testing.
+                    // IMPORTANT: Please replace them with your own OAuth keys for production use.
+                    ThirdPartyNode.Google({
+                      clientId: process.env.GOOGLE_CLIENT_ID,
+                      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                    }),
+                    ThirdPartyNode.Github({
+                      clientId: process.env.GITHUB_CLIENT_ID,
+                      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+                    }),
+                    ThirdPartyNode.Apple({
+                      clientId: process.env.APPLE_CLIENT_ID,
+                      clientSecret: {
+                        keyId: process.env.APPLE_KEY_ID,
+                        privateKey: process.env.APPLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                        teamId: process.env.APPLE_TEAM_ID,
+                      },
+                    }),
+                ],
+            },
+      }),
+      SessionNode.init(),
+    ],
+    isInServerlessEnv: true,
+  }
+}
