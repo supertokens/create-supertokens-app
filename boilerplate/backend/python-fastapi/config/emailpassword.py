d23691870b10b8ee429339201d6c9ff5565ba132
KShivendu <shivendu@iitbhilai.ac.in>
2022-10-28 14:12:11 +0530

    feat: Add FastAPI boilerplate

---
+++ boilerplate/backend/python-fastapi/config/emailpassword.py
@@ -0,0 +1,24 @@
+from supertokens_python.recipe import emailpassword, session
+from supertokens_python import (
+    InputAppInfo,
+    SupertokensConfig,
+)
+
+# this is the location of the SuperTokens core.
+supertokens_config = SupertokensConfig(
+    connection_uri="https://try.supertokens.com")
+
+app_info = InputAppInfo(
+    app_name="Supertokens",
+    api_domain="http://localhost:3001",
+    website_domain="http://localhost:3000",
+)
+
+framework = "fastapi"
+
+# recipeList contains all the modules that you want to
+# use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+recipe_list = [
+    session.init(),
+    emailpassword.init()
+]
