0cd4e6b1da359f3e62d2f18bb99c199fd8efcfa5
Nemi Shah <nemishah1212@gmail.com>
2022-11-03 17:46:33 +0530

    Add cache control header to nextjs boilerplate

--- boilerplate/fullstack/next/pages/api/auth/[[...path]].ts
+++ boilerplate/fullstack/next/pages/api/auth/[[...path]].ts
@@ -9,6 +9,7 @@ supertokens.init(backendConfig());
 export default async function superTokens(req, res) {
     await superTokensNextWrapper(
         async (next) => {
+            res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
             await middleware()(req, res, next);
         },
         req,
