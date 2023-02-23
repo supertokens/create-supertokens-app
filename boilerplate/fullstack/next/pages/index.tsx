f912a266433075448161dd75e9a68eb1bf04cfa2
AreebKhan619 <areebkhan619@gmail.com>
2023-02-23 13:16:57 +0530

    Added NextJS styling

--- boilerplate/fullstack/next/pages/index.tsx
+++ boilerplate/fullstack/next/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/ProtectedHome.module.css";
 import SessionReact from "supertokens-auth-react/recipe/session";
 import SuperTokensReact from "supertokens-auth-react";
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
+import { BlogsIcon, CelebrateIcon, GitHubIcon, GuideIcon, SeparatorLine, SignOutIcon } from "../assets/images";
+import Image from "next/image";
 
 function ProtectedPage() {
     const session = useSessionContext();
@@ -25,111 +27,62 @@ function ProtectedPage() {
         return null;
     }
 
+    function openLink(url: string) {
+        window.open(url, "_blank");
+    }
+
+    const links: { name: string; onClick: () => void; icon: string }[] = [
+        {
+            name: "GitHub",
+            onClick: () => openLink("https://github.com/supertokens/supertokens-auth-react"),
+            icon: GitHubIcon,
+        },
+        {
+            name: "Blogs",
+            onClick: () => openLink("https://supertokens.com/blog"),
+            icon: BlogsIcon,
+        },
+        {
+            name: "Guides",
+            onClick: () => openLink("https://supertokens.com/blog"),
+            icon: GuideIcon,
+        },
+        {
+            name: "Sign Out",
+            onClick: logoutClicked,
+            icon: SignOutIcon,
+        },
+    ];
+
     return (
-        <div className={styles.container}>
+        <div className={styles.homeContainer}>
             <Head>
                 <title>SuperTokens 💫</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-                <p className={styles.description}>You are authenticated with SuperTokens!</p>
-
-                <p className={styles.description}>UserId: {session.userId}</p>
-                <p className={styles.description}>Access token payload: {JSON.stringify(session.accessTokenPayload)}</p>
-                <div
-                    style={{
-                        display: "flex",
-                        height: "70px",
-                        alignItems: "center",
-                        justifyContent: "flex-end",
-                        paddingLeft: "75px",
-                        paddingRight: "75px",
-                    }}
-                >
-                    <div
-                        onClick={logoutClicked}
-                        style={{
-                            display: "flex",
-                            width: "116px",
-                            height: "42px",
-                            backgroundColor: "#000000",
-                            borderRadius: "10px",
-                            cursor: "pointer",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            color: "#ffffff",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        SIGN OUT
+            <div className={styles.mainContainer}>
+                <div className={`${styles.topBand} ${styles.successTitle} ${styles.bold500}`}>
+                    <Image src={CelebrateIcon} alt="Login successful" className={styles.successIcon} /> Login successful
+                </div>
+                <div className={styles.innerContent}>
+                    <div>Your userID is:</div>
+                    <div className={`${styles.truncate} ${styles.userId}`}>{session.userId}</div>
+                    <div onClick={fetchUserData} className={styles.sessionButton}>
+                        Call API
                     </div>
                 </div>
-                <div
-                    style={{
-                        display: "flex",
-                        height: "70px",
-                        alignItems: "center",
-                        justifyContent: "flex-end",
-                        paddingLeft: "75px",
-                        paddingRight: "75px",
-                    }}
-                >
-                    <div
-                        onClick={fetchUserData}
-                        style={{
-                            display: "flex",
-                            width: "150px",
-                            height: "42px",
-                            backgroundColor: "rgb(247 54 54)",
-                            borderRadius: "10px",
-                            cursor: "pointer",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            color: "#ffffff",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        FETCH USER API
+            </div>
+            <div className={styles.bottomLinksContainer}>
+                {links.map((link) => (
+                    <div className={styles.linksContainerLink} key={link.name}>
+                        <Image className={styles.linkIcon} src={link.icon} alt={link.name} />
+                        <div role={"button"} onClick={link.onClick}>
+                            {link.name}
+                        </div>
                     </div>
-                </div>
-
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
-
-                    <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-                        <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-                    </a>
-                </div>
-            </main>
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-                </a>
-            </footer>
+                ))}
+            </div>
+            <Image className={styles.separatorLine} src={SeparatorLine} alt="separator" />
         </div>
     );
 }
