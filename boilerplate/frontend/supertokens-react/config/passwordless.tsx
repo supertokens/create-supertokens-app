a305a5ea1d5e4bea8558768d262ae7eeaf05599c
Nemi Shah <nemishah1212@gmail.com>
2023-02-28 15:31:53 +0530

    Add env to react boilerplate

--- boilerplate/frontend/supertokens-react/config/passwordless.tsx
+++ boilerplate/frontend/supertokens-react/config/passwordless.tsx
@@ -1,11 +1,23 @@
 import Passwordless from "supertokens-auth-react/recipe/passwordless";
 import Session from "supertokens-auth-react/recipe/session";
 
+function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
 export const SuperTokensConfig = {
     appInfo: {
         appName: "SuperTokens Demo App",
-        apiDomain: "http://localhost:3001",
-        websiteDomain: "http://localhost:3000",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
     },
     // recipeList contains all the modules that you want to
     // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
