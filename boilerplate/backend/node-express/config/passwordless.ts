08a2ca1e2d698dc8a702f236549f4d4d36155d98
Nemi Shah <nemishah1212@gmail.com>
2023-02-28 16:27:37 +0530

    Use env instead of hardcoded urls

--- boilerplate/backend/node-express/config/passwordless.ts
+++ boilerplate/backend/node-express/config/passwordless.ts
@@ -3,13 +3,13 @@ import Session from "supertokens-node/recipe/session";
 import { TypeInput } from "supertokens-node/types";
 import Dashboard from "supertokens-node/recipe/dashboard";
 
-function getApiDomain() {
+export function getApiDomain() {
     const apiPort = process.env.REACT_APP_API_PORT || 3001;
     const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
     return apiUrl;
 }
 
-function getWebsiteDomain() {
+export function getWebsiteDomain() {
     const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
     const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
     return websiteUrl;
