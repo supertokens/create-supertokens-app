dc3a0002f3e35bd12496607efde174500fd01d6c
Nemi Shah <nemishah1212@gmail.com>
2023-07-21 10:26:04 +0530

    Temporarily disabled go and python

--- boilerplate/backend/node-express/config/thirdpartypasswordless.ts
+++ boilerplate/backend/node-express/config/thirdpartypasswordless.ts
@@ -39,7 +39,7 @@ export const SuperTokensConfig: TypeInput = {
                             {
                                 clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
                                 clientSecret: "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",
-                            }
+                            },
                         ],
                     },
                 },
@@ -50,7 +50,7 @@ export const SuperTokensConfig: TypeInput = {
                             {
                                 clientId: "467101b197249757c71f",
                                 clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
-                            }
+                            },
                         ],
                     },
                 },
@@ -66,7 +66,7 @@ export const SuperTokensConfig: TypeInput = {
                                         "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
                                     teamId: "YWQCXGJRJL",
                                 },
-                            }
+                            },
                         ],
                     },
                 },
