9c4d7596b75a36230b8f81716e39ec649be8c5e2
rishabhpoddar <rishabh.poddar@gmail.com>
2023-07-21 21:40:21 +0530

    nextjs boilerplate fix

--- boilerplate/fullstack/next/config/backend/thirdparty.ts
+++ boilerplate/fullstack/next/config/backend/thirdparty.ts
@@ -20,22 +20,45 @@ export let backendConfig = (): AuthConfig => {
                     providers: [
                         // We have provided you with development keys which you can use for testing.
                         // IMPORTANT: Please replace them with your own OAuth keys for production use.
-                        ThirdPartyNode.Google({
-                            clientId: process.env.GOOGLE_CLIENT_ID,
-                            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                        }),
-                        ThirdPartyNode.Github({
-                            clientId: process.env.GITHUB_CLIENT_ID,
-                            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-                        }),
-                        ThirdPartyNode.Apple({
-                            clientId: process.env.APPLE_CLIENT_ID,
-                            clientSecret: {
-                                keyId: process.env.APPLE_KEY_ID,
-                                privateKey: process.env.APPLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-                                teamId: process.env.APPLE_TEAM_ID,
+                        {
+                            config: {
+                                thirdPartyId: "google",
+                                clients: [
+                                    {
+                                        clientId:
+                                            "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+                                        clientSecret: "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",
+                                    },
+                                ],
                             },
-                        }),
+                        },
+                        {
+                            config: {
+                                thirdPartyId: "github",
+                                clients: [
+                                    {
+                                        clientId: "467101b197249757c71f",
+                                        clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+                                    },
+                                ],
+                            },
+                        },
+                        {
+                            config: {
+                                thirdPartyId: "apple",
+                                clients: [
+                                    {
+                                        clientId: "4398792-io.supertokens.example.service",
+                                        additionalConfig: {
+                                            keyId: "7M48Y4RYDL",
+                                            privateKey:
+                                                "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+                                            teamId: "YWQCXGJRJL",
+                                        },
+                                    },
+                                ],
+                            },
+                        },
                     ],
                 },
             }),
