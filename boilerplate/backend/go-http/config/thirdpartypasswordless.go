d584bba1b43f041dbb432692833a5335973094ac
Nemi Shah <nemishah1212@gmail.com>
2023-08-25 21:13:05 +0530

    Fix third party configs for golang boilerplate

--- boilerplate/backend/go-http/config/thirdpartypasswordless.go
+++ boilerplate/backend/go-http/config/thirdpartypasswordless.go
@@ -4,7 +4,6 @@ import (
 	"github.com/supertokens/supertokens-golang/recipe/dashboard"
 	"github.com/supertokens/supertokens-golang/recipe/passwordless/plessmodels"
 	"github.com/supertokens/supertokens-golang/recipe/session"
-	"github.com/supertokens/supertokens-golang/recipe/thirdparty"
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
 	"github.com/supertokens/supertokens-golang/recipe/thirdpartypasswordless"
 	"github.com/supertokens/supertokens-golang/recipe/thirdpartypasswordless/tplmodels"
@@ -26,23 +25,44 @@ var SuperTokensConfig = supertokens.TypeInput{
 			ContactMethodEmailOrPhone: plessmodels.ContactMethodEmailOrPhoneConfig{
 				Enabled: true,
 			},
-			Providers: []tpmodels.TypeProvider{
-				thirdparty.Google(tpmodels.GoogleConfig{
-					ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-					ClientSecret: "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",
-				}),
-				thirdparty.Github(tpmodels.GithubConfig{
-					ClientID:     "467101b197249757c71f",
-					ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
-				}),
-				thirdparty.Apple(tpmodels.AppleConfig{
-					ClientID: "4398792-io.supertokens.example.service",
-					ClientSecret: tpmodels.AppleClientSecret{
-						KeyId:      "7M48Y4RYDL",
-						PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-						TeamId:     "YWQCXGJRJL",
+			Providers: []tpmodels.ProviderInput{
+				{
+					Config: tpmodels.ProviderConfig{
+						ThirdPartyId: "google",
+						Clients: []tpmodels.ProviderClientConfig{
+							{
+								ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+								ClientSecret: "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",
+							},
+						},
 					},
-				}),
+				},
+				{
+					Config: tpmodels.ProviderConfig{
+						ThirdPartyId: "github",
+						Clients: []tpmodels.ProviderClientConfig{
+							{
+								ClientID:     "467101b197249757c71f",
+								ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+							},
+						},
+					},
+				},
+				{
+					Config: tpmodels.ProviderConfig{
+						ThirdPartyId: "apple",
+						Clients: []tpmodels.ProviderClientConfig{
+							{
+								ClientID: "4398792-io.supertokens.example.service",
+								AdditionalConfig: map[string]interface{}{
+									"keyId":      "7M48Y4RYDL",
+									"privateKey": "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+									"teamId":     "YWQCXGJRJL",
+								},
+							},
+						},
+					},
+				},
 			},
 		}),
 		session.Init(nil), // initializes session features
