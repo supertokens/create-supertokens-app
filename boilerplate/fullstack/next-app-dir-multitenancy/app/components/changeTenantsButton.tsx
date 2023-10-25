2dc1014439a37d63e8caaa4a319c4242236ea390
Nemi Shah <nemishah1212@gmail.com>
2023-10-25 10:27:15 +0530

    Add logic to create a multitenant app for next with app dir

--- boilerplate/fullstack/next-app-dir-multitenancy/app/components/changeTenantsButton.tsx
+++ boilerplate/fullstack/next-app-dir-multitenancy/app/components/changeTenantsButton.tsx
@@ -1,6 +1,8 @@
 // We display this component as part of the SuperTokens login form to
 // allow users to go back and select another tenant without logging in
-export const ChangeTenantsButton = (props: { setHasSelectedTenantId: React.Dispatch<React.SetStateAction<boolean>> }) => {
+export const ChangeTenantsButton = (props: {
+    setHasSelectedTenantId: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
     return (
         <div
             data-supertokens="link tenants-link"
