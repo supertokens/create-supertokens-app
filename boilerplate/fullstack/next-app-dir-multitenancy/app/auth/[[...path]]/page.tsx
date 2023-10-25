c6ea67c20fbe2badf34bd3dd1e6a5f0ef72c1f8e
Nemi Shah <nemishah1212@gmail.com>
2023-10-25 10:19:05 +0530

    Add example app for multitenancy with next js ap directory

---
+++ boilerplate/fullstack/next-app-dir-multitenancy/app/auth/[[...path]]/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { redirectToAuth } from "supertokens-auth-react";
+import SuperTokens from "supertokens-auth-react/ui";
+import { PreBuiltUIList } from "../../config/frontend";
+import { useSessionContext } from "supertokens-auth-react/recipe/session";
+import { ThirdpartyEmailPasswordComponentsOverrideProvider } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import { ThirdpartyPasswordlessComponentsOverrideProvider } from "supertokens-auth-react/recipe/thirdpartypasswordless";
+import { ChangeTenantsButton } from "../../../app/components/changeTenantsButton";
+import { TenantSelector } from "../../../app/components/TenantSelector";
+
+export default function Auth() {
+    if (typeof window === "undefined") {
+        return null;
+    }
+
+    const tenantId = localStorage.getItem("tenantId");
+    const session = useSessionContext();
+    const [hasSelectedTenantId, setHasSelectedTenantId] = React.useState(false);
+
+    // if the user visits a page that is not handled by us (like /auth/random), then we redirect them back to the auth page.
+    useEffect(() => {
+        if (SuperTokens.canHandleRoute(PreBuiltUIList) === false) {
+            redirectToAuth({ redirectBack: false });
+        }
+    }, []);
+
+    if (session.loading === true) {
+        return null;
+    }
+
+    const SuperTokensComponent = SuperTokens.getRoutingComponent(PreBuiltUIList);
+
+    if (
+        hasSelectedTenantId ||
+        tenantId !== null ||
+        session.doesSessionExist === true ||
+        new URLSearchParams(location.search).has("tenantId")
+    ) {
+        return (
+            <ThirdpartyEmailPasswordComponentsOverrideProvider
+                components={{
+                    EmailPasswordSignInFooter_Override: ({ DefaultComponent, ...props }) => {
+                        return (
+                            <div>
+                                <DefaultComponent {...props} />
+                                <ChangeTenantsButton setHasSelectedTenantId={setHasSelectedTenantId} />
+                            </div>
+                        );
+                    },
+                }}
+            >
+                <ThirdpartyPasswordlessComponentsOverrideProvider
+                    components={{
+                        PasswordlessSignInUpFooter_Override: ({ DefaultComponent, ...props }) => {
+                            return (
+                                <div>
+                                    <DefaultComponent {...props} />
+                                    <ChangeTenantsButton setHasSelectedTenantId={setHasSelectedTenantId} />
+                                </div>
+                            );
+                        },
+                    }}
+                >
+                    {SuperTokensComponent}
+                </ThirdpartyPasswordlessComponentsOverrideProvider>
+            </ThirdpartyEmailPasswordComponentsOverrideProvider>
+        );
+    } else {
+        return <TenantSelector setHasSelectedTenantId={setHasSelectedTenantId} />;
+    }
+}
---
+++ boilerplate/fullstack/next-app-dir-multitenancy/app/config/backend/multitenancy.ts
@@ -0,0 +1,37 @@
+import ThirdPartyEmailPasswordNode from "supertokens-node/recipe/thirdpartyemailpassword";
+import ThirdPartyPasswordlessNode from "supertokens-node/recipe/thirdpartypasswordless";
+import SessionNode from "supertokens-node/recipe/session";
+import Dashboard from "supertokens-node/recipe/dashboard";
+import { appInfo } from "./appinfo";
+import { TypeInput } from "supertokens-node/types";
+import SuperTokens from "supertokens-node";
+
+export let backendConfig = (): TypeInput => {
+    return {
+        supertokens: {
+            // this is the location of the SuperTokens core.
+            connectionURI: "https://try.supertokens.com",
+        },
+        appInfo,
+        // recipeList contains all the modules that you want to
+        // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+        recipeList: [
+            ThirdPartyEmailPasswordNode.init(),
+            ThirdPartyPasswordlessNode.init({
+                contactMethod: "EMAIL",
+                flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+            }),
+            SessionNode.init(),
+            Dashboard.init(),
+        ],
+        isInServerlessEnv: true,
+    };
+};
+
+let initialized = false;
+export function ensureSuperTokensInit() {
+    if (!initialized) {
+        SuperTokens.init(backendConfig());
+        initialized = true;
+    }
+}
