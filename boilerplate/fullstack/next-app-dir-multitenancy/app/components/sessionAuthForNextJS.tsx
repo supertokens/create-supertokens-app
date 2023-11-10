40859fa66033df7741bae88f5a5e841bedce0671
rishabhpoddar <rishabh.poddar@gmail.com>
2023-11-10 11:03:11 +0530

    fixes nextjs sessionauth wrapper

--- boilerplate/fullstack/next-app-dir-multitenancy/app/components/sessionAuthForNextJS.tsx
+++ boilerplate/fullstack/next-app-dir-multitenancy/app/components/sessionAuthForNextJS.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import { SessionAuth } from "supertokens-auth-react/recipe/session";
 
 type Props = Parameters<typeof SessionAuth>[0] & {
@@ -7,5 +8,12 @@ type Props = Parameters<typeof SessionAuth>[0] & {
 };
 
 export const SessionAuthForNextJS = (props: Props) => {
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
+    if (!loaded) {
+        return props.children;
+    }
     return <SessionAuth {...props}>{props.children}</SessionAuth>;
 };
