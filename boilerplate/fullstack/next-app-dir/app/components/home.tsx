2e938e44945228ee23314805a92d2003c2ff4ba4
Mihaly Lengyel <mihaly@lengyel.tech>
2023-11-13 08:45:11 +0100

    feat: update ssrSessionExists usage to match name better

--- boilerplate/fullstack/next-app-dir/app/components/home.tsx
+++ boilerplate/fullstack/next-app-dir/app/components/home.tsx
@@ -21,7 +21,7 @@ export async function HomePage() {
         }
 
         if (hasInvalidClaims) {
-            return <SessionAuthForNextJS ssrSessionExists={false} />;
+            return <SessionAuthForNextJS ssrSessionExists={true} />;
         } else {
             return <TryRefreshComponent />;
         }
