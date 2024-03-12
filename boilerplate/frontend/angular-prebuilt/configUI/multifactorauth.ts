7cec1d69aee0a34b14145f2b7ba23f2fb78095bf
Mihaly Lengyel <mihaly@lengyel.tech>
2024-03-12 11:15:23 +0100

    feat: add mfa config for vue and angular

---
+++ boilerplate/frontend/angular-prebuilt/configUI/multifactorauth.ts
@@ -0,0 +1,5 @@
+import { ThirdPartyEmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/thirdpartyemailpassword/prebuiltui";
+import { MultiFactorAuthPreBuiltUI } from "supertokens-auth-react/recipe/multifactorauth/prebuiltui";
+import { TOTPPreBuiltUI } from "supertokens-auth-react/recipe/totp/prebuiltui";
+
+export const PreBuiltUIList = [ThirdPartyEmailPasswordPreBuiltUI, MultiFactorAuthPreBuiltUI, TOTPPreBuiltUI];
