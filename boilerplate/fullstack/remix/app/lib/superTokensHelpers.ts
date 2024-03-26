7cf82a47f876543b79a65a1a68b21f2839f6ee95
Darko Bozhinovski <hello@darko.io>
2024-03-26 14:14:45 +0100

    feat: ports withSession to remix

--- boilerplate/fullstack/remix/app/lib/superTokensHelpers.ts
+++ boilerplate/fullstack/remix/app/lib/superTokensHelpers.ts
@@ -1,9 +1,16 @@
-import { PreParsedRequest, CollectingResponse, middleware } from "supertokens-node/framework/custom/index.js";
+import {
+    PreParsedRequest,
+    CollectingResponse,
+    middleware,
+    errorHandler,
+} from "supertokens-node/framework/custom/index.js";
 import Session, { SessionContainer, VerifySessionOptions } from "supertokens-node/recipe/session/index.js";
 import SessionRecipe from "supertokens-node/lib/build/recipe/session/recipe.js";
 import { availableTokenTransferMethods } from "supertokens-node/lib/build/recipe/session/constants.js";
 import { getToken } from "supertokens-node/lib/build/recipe/session/cookieAndHeaders.js";
 import { parseJWTWithoutSignatureVerification } from "supertokens-node/lib/build/recipe/session/jwt.js";
+import { UserContext } from "supertokens-node/types";
+import { getUserContext } from "supertokens-node/lib/build/utils.js";
 import { serialize } from "cookie";
 
 type HTTPMethod = "post" | "get" | "delete" | "put" | "options" | "trace";
@@ -137,5 +144,149 @@ export async function getSessionDetails(
         } else {
             throw err;
         }
+    }
+}
+
+async function commonSSRSession(
+    baseRequest: PreParsedRequest,
+    options: VerifySessionOptions | undefined,
+    userContext: UserContext
+): Promise<{
+    session: SessionContainer | undefined;
+    hasToken: boolean;
+    hasInvalidClaims: boolean;
+    baseResponse: CollectingResponse;
+    remixResponse?: Response;
+}> {
+    const baseResponse = new CollectingResponse();
+
+    // Possible interop issue.
+    const recipe = (SessionRecipe as any).default.instance;
+    const tokenTransferMethod = recipe.config.getTokenTransferMethod({
+        req: baseRequest,
+        forCreateNewSession: false,
+        userContext,
+    });
+    const transferMethods = tokenTransferMethod === "any" ? availableTokenTransferMethods : [tokenTransferMethod];
+    const hasToken = transferMethods.some((transferMethod) => {
+        const token = getToken(baseRequest, "access", transferMethod);
+        if (!token) {
+            return false;
+        }
+
+        try {
+            parseJWTWithoutSignatureVerification(token);
+            return true;
+        } catch {
+            return false;
+        }
+    });
+
+    try {
+        const session = await Session.getSession(baseRequest, baseResponse, options, userContext);
+        return {
+            session,
+            hasInvalidClaims: false,
+            hasToken,
+            baseResponse,
+        };
+    } catch (err) {
+        if (Session.Error.isErrorFromSuperTokens(err)) {
+            return {
+                hasToken,
+                hasInvalidClaims: err.type === Session.Error.INVALID_CLAIMS,
+                session: undefined,
+                baseResponse,
+                remixResponse: new Response("Authentication required", {
+                    status: err.type === Session.Error.INVALID_CLAIMS ? 403 : 401,
+                }),
+            };
+        } else {
+            throw err;
+        }
+    }
+}
+
+export async function withSession<Request, Response>(
+    req: Request,
+    handler: (error: Error | undefined, session: SessionContainer | undefined) => Promise<Response>,
+    options?: VerifySessionOptions,
+    userContext?: Record<string, any>
+): Promise<Response> {
+    try {
+        const query = getQueryFromRequest(req);
+        const cookies = getCookieFromRequest(req);
+
+        const baseRequest = new PreParsedRequest({
+            method: req.method as HTTPMethod,
+            url: req.url,
+            query: query,
+            headers: req.headers,
+            cookies: cookies,
+            getFormBody: () => req!.formData(),
+            getJSONBody: () => req!.json(),
+        });
+
+        const { session, remixResponse, baseResponse } = await commonSSRSession(
+            baseRequest,
+            options,
+            getUserContext(userContext)
+        );
+
+        if (remixResponse) {
+            return remixResponse as Response;
+        }
+
+        let userResponse: Response;
+
+        try {
+            userResponse = await handler(undefined, session);
+        } catch (err) {
+            await errorHandler()(err, baseRequest, baseResponse, (errorHandlerError: Error) => {
+                if (errorHandlerError) {
+                    throw errorHandlerError;
+                }
+            });
+
+            // The headers in the userResponse are set twice from baseResponse, but the resulting response contains unique headers.
+            userResponse = new Response(baseResponse.body, {
+                status: baseResponse.statusCode,
+                headers: baseResponse.headers,
+            }) as Response;
+
+            let didAddCookies = false;
+            let didAddHeaders = false;
+
+            for (const respCookie of baseResponse.cookies) {
+                didAddCookies = true;
+                userResponse.headers.append(
+                    "Set-Cookie",
+                    serialize(respCookie.key, respCookie.value, {
+                        domain: respCookie.domain,
+                        expires: new Date(respCookie.expires),
+                        httpOnly: respCookie.httpOnly,
+                        path: respCookie.path,
+                        sameSite: respCookie.sameSite,
+                        secure: respCookie.secure,
+                    })
+                );
+            }
+
+            baseResponse.headers.forEach((value: string, key: string) => {
+                didAddHeaders = true;
+                userResponse.headers.set(key, value);
+            });
+            if (didAddCookies || didAddHeaders) {
+                if (!userResponse.headers.has("Cache-Control")) {
+                    // This is needed for production deployments with Vercel
+                    userResponse.headers.set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+                }
+            }
+        }
+
+        return userResponse;
+    } catch (error) {
+        console.error(error);
+        return await handler(error as Error, undefined);
     }
 }
--- boilerplate/fullstack/remix/app/routes/sessioninfo.$.tsx
+++ boilerplate/fullstack/remix/app/routes/sessioninfo.$.tsx
@@ -1,15 +1,22 @@
 import { json } from "@remix-run/node";
 import { LoaderFunctionArgs } from "react-router-dom";
-import { getSessionDetails } from "../lib/superTokensHelpers.js";
+import { withSession } from "../lib/superTokensHelpers.js";
 
 // Loader function for handling GET requests that also adds cache control headers
 export async function loader({ request }: LoaderFunctionArgs) {
-    try {
-        const res = await getSessionDetails(request);
-        return {
-            session: res.session,
-        };
-    } catch (error) {
-        return json({ error: "Internal server error" }, { status: 500 });
-    }
+    return withSession(request, async (err, session) => {
+        if (err) {
+            return json(err, { status: 500 });
+        }
+        if (!session) {
+            return new Response("Authentication required", { status: 401 });
+        }
+
+        return json({
+            note: "Fetch any data from your application for authenticated user after using verifySession middleware",
+            userId: session.getUserId(),
+            sessionHandle: session.getHandle(),
+            accessTokenPayload: session.getAccessTokenPayload(),
+        });
+    });
 }
