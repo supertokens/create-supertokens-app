e8aea6fba157907266a8b0f95ed10c63d46dca78
rishabhpoddar <rishabh.poddar@gmail.com>
2024-05-13 11:56:51 +0530

    fixes tpep config for golang boilerplate

--- boilerplate/backend/go-http/config/thirdpartyemailpassword.go
+++ boilerplate/backend/go-http/config/thirdpartyemailpassword.go
@@ -20,32 +20,16 @@ var SuperTokensConfig = supertokens.TypeInput{
 	},
 	RecipeList: []supertokens.Recipe{
 		thirdpartyemailpassword.Init(&tpepmodels.TypeInput{
-			/*
-			   We use different credentials for different platforms when required. For example the redirect URI for Github
-			   is different for Web and mobile. In such a case we can provide multiple providers with different client Ids.
-
-			   When the frontend makes a request and wants to use a specific clientId, it needs to send the clientId to use in the
-			   request. In the absence of a clientId in the request the SDK uses the default provider, indicated by `isDefault: true`.
-			   When adding multiple providers for the same type (Google, Github etc), make sure to set `isDefault: true`.
-			*/
 			Providers: []tpmodels.ProviderInput{
-				// We have provided you with development keys which you can use for testsing.
+				// We have provided you with development keys which you can use for testing.
 				// IMPORTANT: Please replace them with your own OAuth keys for production use.
 				{
 					Config: tpmodels.ProviderConfig{
 						ThirdPartyId: "google",
 						Clients: []tpmodels.ProviderClientConfig{
-							// We use this for websites
 							{
 								ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
 								ClientSecret: "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",
-								ClientType: "web",
-							},
-							// we use this for mobile apps
-							{
-								ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-								ClientSecret: "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",
-								ClientType:  "mobile",
 							},
 						},
 					},
@@ -54,47 +38,9 @@ var SuperTokensConfig = supertokens.TypeInput{
 					Config: tpmodels.ProviderConfig{
 						ThirdPartyId: "github",
 						Clients: []tpmodels.ProviderClientConfig{
-							// We use this for websites
 							{
 								ClientID:     "467101b197249757c71f",
 								ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
-								ClientType: "web",
-							},
-							// We use this for mobile apps
-							{
-								ClientID:     "8a9152860ce869b64c44",
-								ClientSecret: "00e841f10f288363cd3786b1b1f538f05cfdbda2",
-							},
-						},
-					},
-				},
-				/*
-				   For Apple signin, iOS apps always use the bundle identifier as the client ID when communicating with Apple. Android, Web and other platforms
-				   need to configure a Service ID on the Apple developer dashboard and use that as client ID.
-				   In the example below 4398792-io.supertokens.example.service is the client ID for Web. Android etc and thus we mark it as default. For iOS
-				   the frontend for the demo app sends the clientId in the request which is then used by the SDK.
-				*/
-				{
-					Config: tpmodels.ProviderConfig{
-						ThirdPartyId: "apple",
-						Clients: []tpmodels.ProviderClientConfig{
-							// For Android and website apps
-							{
-								ClientID: "4398792-io.supertokens.example.service",
-								AdditionalConfig: map[string]interface{}{
-									"keyId":      "7M48Y4RYDL",
-									"privateKey": "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-									"teamId":     "YWQCXGJRJL",
-								},
-							},
-							// For iOS Apps
-							{
-								ClientID: "4398792-io.supertokens.example",
-								AdditionalConfig: map[string]interface{}{
-									"keyId":      "7M48Y4RYDL",
-									"privateKey": "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-									"teamId":     "YWQCXGJRJL",
-								},
 							},
 						},
 					},
@@ -106,6 +52,21 @@ var SuperTokensConfig = supertokens.TypeInput{
 							{
 								ClientID:     "4398792-WXpqVXRiazdRMGNJdEZIa3RVQXc6MTpjaQ",
 								ClientSecret: "BivMbtwmcygbRLNQ0zk45yxvW246tnYnTFFq-LH39NwZMxFpdC",
+							},
+						},
+					},
+				},
+				{
+					Config: tpmodels.ProviderConfig{
+						ThirdPartyId: "apple",
+						Clients: []tpmodels.ProviderClientConfig{
+							{
+								ClientID: "4398792-io.supertokens.example.service",
+								AdditionalConfig: map[string]interface{}{
+									"keyId":      "7M48Y4RYDL",
+									"privateKey": "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+									"teamId":     "YWQCXGJRJL",
+								},
 							},
 						},
 					},
