abcaa87c3f8a9bd40a23dd1bd4ca35fd030df348
rishabhpoddar <rishabh.poddar@gmail.com>
2024-08-10 12:22:33 +0530

    cicd

--- .circleci/updateApps.sh
+++ .circleci/updateApps.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/supertokens/live-apps.git
+nvm use 16
 
 cd live-apps 
 
@@ -23,6 +24,20 @@ else
     echo "Error: thirdpartyemailpassword directory not created"
     exit 1
 fi
+
+rm -rf thirdparty
+npx create-supertokens-app@latest --frontend=next-app-directory --recipe=thirdparty --appname=thirdparty
+if [ -d "thirdparty" ]; then
+    cd thirdparty
+    find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://thirdparty.demo.supertokens.com|g' {} +
+    cd ../
+else
+    echo "Error: thirdparty directory not created"
+    exit 1
+fi
+
+git config --global user.email "$EMAIL"
+git config --global user.name "$NAME"
 
 git add --all
 git commit -m "updates to example apps"
