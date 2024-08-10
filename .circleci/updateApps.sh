b58cc5e70f2d4f7878dc4c6c2f72ae2dc4e063a8
rishabhpoddar <rishabh.poddar@gmail.com>
2024-08-10 12:06:11 +0530

    cicd

--- .circleci/updateApps.sh
+++ .circleci/updateApps.sh
@@ -3,17 +3,27 @@ git clone https://github.com/supertokens/live-apps.git
 cd live-apps 
 
 rm -rf emailpassword
-npx -y create-supertokens-app@latest --frontend=next-app-directory --recipe=emailpassword --appname=emailpassword
-cd emailpassword
-find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://emailpassword.demo.supertokens.com|g' {} +
-cd ../
+npx create-supertokens-app@latest --frontend=next-app-directory --recipe=emailpassword --appname=emailpassword
+if [ -d "emailpassword" ]; then
+    cd emailpassword
+    find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://emailpassword.demo.supertokens.com|g' {} +
+    cd ../
+else
+    echo "Error: emailpassword directory not created"
+    exit 1
+fi
 
 rm -rf thirdpartyemailpassword
-npx -y create-supertokens-app@latest --frontend=next-app-directory --recipe=thirdpartyemailpassword --appname=thirdpartyemailpassword
-cd thirdpartyemailpassword
-find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://thirdpartyemailpassword.demo.supertokens.com|g' {} +
-cd ../
+npx create-supertokens-app@latest --frontend=next-app-directory --recipe=thirdpartyemailpassword --appname=thirdpartyemailpassword
+if [ -d "thirdpartyemailpassword" ]; then
+    cd thirdpartyemailpassword
+    find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://thirdpartyemailpassword.demo.supertokens.com|g' {} +
+    cd ../
+else
+    echo "Error: thirdpartyemailpassword directory not created"
+    exit 1
+fi
 
 git add --all
-git commit -m"updates to example apps"
+git commit -m "updates to example apps"
 git push
