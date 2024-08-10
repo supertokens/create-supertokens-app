cce5dcc528fb79d2aaff4352fbcbe13333a8382e
rishabhpoddar <rishabh.poddar@gmail.com>
2024-08-10 12:37:58 +0530

    cicd

--- .circleci/updateApps.sh
+++ .circleci/updateApps.sh
@@ -35,6 +35,39 @@ else
     exit 1
 fi
 
+rm -rf passwordless
+npx create-supertokens-app@latest --frontend=next-app-directory --recipe=passwordless --appname=passwordless
+if [ -d "passwordless" ]; then
+    cd passwordless
+    find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://passwordless.demo.supertokens.com|g' {} +
+    cd ../
+else
+    echo "Error: passwordless directory not created"
+    exit 1
+fi
+
+rm -rf thirdpartypasswordless
+npx create-supertokens-app@latest --frontend=next-app-directory --recipe=thirdpartypasswordless --appname=thirdpartypasswordless
+if [ -d "thirdpartypasswordless" ]; then
+    cd thirdpartypasswordless
+    find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://thirdpartypasswordless.demo.supertokens.com|g' {} +
+    cd ../
+else
+    echo "Error: thirdpartypasswordless directory not created"
+    exit 1
+fi
+
+rm -rf dashboard-demo
+npx create-supertokens-app@latest --dashboardDemo=true
+if [ -d "dashboard-demo" ]; then
+    cd dashboard-demo
+    find . -type f -not -path '*/node_modules/*' -exec sed -i 's|http://localhost:3000|https://dashboard.demo.supertokens.com|g' {} +
+    cd ../
+else
+    echo "Error: dashboard-demo directory not created"
+    exit 1
+fi
+
 git config --global user.email "$EMAIL"
 git config --global user.name "$NAME"
 
