760cbb4e445cc76be1731efce406fcc44117632f
Bijit-Mondal <bijitmondal3011@rediffmail.com>
2024-10-02 10:56:10 +0530

    added dynamic port express js

--- boilerplate/backend/node-express/config/thirdpartyemailpassword.ts
+++ boilerplate/backend/node-express/config/thirdpartyemailpassword.ts
@@ -6,13 +6,13 @@ import Dashboard from "supertokens-node/recipe/dashboard";
 import UserRoles from "supertokens-node/recipe/userroles";
 
 export function getApiDomain() {
-    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiPort = process.env.REACT_APP_API_PORT || process.env.VITE_API_PORT || 3001;
     const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
     return apiUrl;
 }
 
 export function getWebsiteDomain() {
-    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || process.env.VITE_APP_PORT || 3000;
     const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
     return websiteUrl;
 }
