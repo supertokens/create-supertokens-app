473df43b757989a379f11830756c80131219b6bf
ceroy-ak <abhishek1091998@gmail.com>
2024-10-10 18:59:15 +0530

    fixes

--- boilerplate/frontend/supertokens-react-custom/src/pages/Home/index.tsx
+++ boilerplate/frontend/supertokens-react-custom/src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Hello World from Supertokens</h1>
-        </div>
-    );
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/dashboard");
+    }, []);
+    return <></>;
 }
