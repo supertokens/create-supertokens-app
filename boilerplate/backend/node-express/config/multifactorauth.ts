532228e282692a0345de13edd04e31a0f1134571
connor <connor@connors-MacBook-Pro.local>
2024-12-15 16:08:49 -0600

    change environmental variable names

--- boilerplate/backend/node-express/config/multifactorauth.ts
+++ boilerplate/backend/node-express/config/multifactorauth.ts
@@ -10,14 +10,14 @@ import EmailVerification from "supertokens-node/recipe/emailverification";
 import TOTP from "supertokens-node/recipe/totp";
 
 export function getApiDomain() {
-    const apiPort = process.env.REACT_APP_API_PORT || 3001;
-    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    const apiPort = process.env.VITE_APP_API_PORT || 3001;
+    const apiUrl = process.env.VITE_APP_API_URL || `http://localhost:${apiPort}`;
     return apiUrl;
 }
 
 export function getWebsiteDomain() {
-    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
-    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    const websitePort = process.env.VITE_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.VITE_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
     return websiteUrl;
 }
 
