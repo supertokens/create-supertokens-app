6e8d7357638374819cd6c9b3c32ed2c77f6e4bb9
connor <connor@connors-MacBook-Pro.local>
2024-12-17 20:55:42 -0600

    feat: update koa boilerplate as scaffolding

--- boilerplate/backend/koa/config/all_auth.ts
+++ boilerplate/backend/koa/config/all_auth.ts
@@ -1,19 +1,35 @@
 import EmailPassword from "supertokens-node/recipe/emailpassword";
 import Session from "supertokens-node/recipe/session";
 import Dashboard from "supertokens-node/recipe/dashboard";
-import { appInfo } from "./appInfo";
 import ThirdParty from "supertokens-node/recipe/thirdparty";
 import Passwordless from "supertokens-node/recipe/passwordless";
 import { TypeInput } from "supertokens-node/types";
 import UserRoles from "supertokens-node/recipe/userroles";
-import SuperTokens from "supertokens-node";
+
+export function getApiDomain() {
+    const apiPort = process.env.VITE_APP_API_PORT || 3001;
+    const apiUrl = process.env.VITE_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.VITE_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.VITE_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
 
 export const SuperTokensConfig: TypeInput = {
     framework: "koa",
     supertokens: {
+        // this is the location of the SuperTokens core.
         connectionURI: "https://try.supertokens.com",
     },
-    appInfo,
+    appInfo: {
+        // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+        appName: "SuperTokens Koa demo app",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
+    },
     // recipeList contains all the modules that you want to
     // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
     recipeList: [
