c7c3c8434fbdc54f5b492a50744172cf3fb7d217
connor <connor@connors-MacBook-Pro.local>
2025-02-03 11:33:29 -0600

    Update to new styling.

--- boilerplate/frontend/angular/src/app/home/home.component.ts
+++ boilerplate/frontend/angular/src/app/home/home.component.ts
@@ -1,9 +1,25 @@
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import * as Session from "supertokens-web-js/recipe/session";
 
 @Component({
     selector: "app-home",
-    imports: [],
+    imports: [CommonModule],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+    public session = false;
+
+    ngAfterViewInit() {
+        this.getSessionInfo();
+    }
+
+    async getSessionInfo() {
+        this.session = await Session.doesSessionExist();
+    }
+
+    redirect(location: string) {
+        window.location.href = location;
+    }
+}
